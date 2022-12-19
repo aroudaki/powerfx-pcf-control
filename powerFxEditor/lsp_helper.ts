03eaca8fdebbcec23526f33d96df3fc1f6246cce
Ali Roudaki <72997345+aroudaki@users.noreply.github.com>
2022-12-19 13:46:40 -0800

    remove old code

--- powerFxEditor/lsp_helper.ts
+++
@@ -1,7 +0,0 @@
-export async function sendDataAsync(LSP_URL: string, endpoint: string, data: string): Promise<Response> {
-  return await fetch(LSP_URL + endpoint, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: data
-  });
-}
